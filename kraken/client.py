cc7154f1ccae3792516e01a5741920a1ca9d5b8f
Fisher60 <wakefollower@gmail.com>
2023-11-08 19:11:51 -0600

    Add all endpoints, switch to PDM

--- kraken/client.py
+++ kraken/client.py
@@ -1,4 +1,8 @@
+from time import time
+
 from httpx import AsyncClient, RequestError
+
+from kraken.models import TradeableAssetPairsInfo
 
 
 class KrakenClient:
@@ -10,14 +14,99 @@ class KrakenClient:
         self.private_api_url = f"{base_api_url}/private"
         self.public_api_url = f"{base_api_url}/public"
 
-    async def _get_public(self, resource_path):
+    async def _get_public(self, resource_path: str, key: str | None = None, params: dict | None = None):
         async with AsyncClient() as client:
-            data = (await client.get(f"{self.public_api_url}/{resource_path}")).json()
+            resp = (await client.get(f"{self.public_api_url}/{resource_path}", params=params)).json()
 
-        if errors := data.get("error"):
+        if errors := resp.get("error"):
             raise RequestError(errors)
 
-        return data
+        result = resp["result"]
+
+        if key is not None:
+            try:
+                return result.get(key)
+            except KeyError:
+                raise KeyError(f"Key '{key}' not in response result")
+        else:
+            return result
 
     async def get_server_time(self):
-        return (await self._get_public("Time")).get("unixtime")
+        return await self._get_public("Time", "unixtime")
+
+    async def get_system_status(self):
+        return await self._get_public("SystemStatus", "status")
+
+    async def get_assets_info(self, assets: list[str] | None = None, aclass: str = "currency"):
+        params = {
+            "aclass": aclass
+        }
+
+        if assets is not None:
+            params["asset"] = ",".join([x.upper() for x in assets])
+
+        return await self._get_public("Assets", params=params)
+
+    async def get_tradable_asset_pairs(
+            self,
+            pairs: list[str],
+            info: TradeableAssetPairsInfo = TradeableAssetPairsInfo.info
+    ):
+        """Each ticker_pair is a ticker_pair of currencies such as 'BTC/USD' or 'ETH/BTC'"""
+        params = {
+            "ticker_pair": ",".join(pairs),
+            "info": info
+        }
+        return await self._get_public("AssetPairs", params=params)
+
+    async def get_ticker_information(self, ticker_pair: str | None):
+        """Ticker pair is a valid combination of asset codes as outlined in kraken support
+
+        https://support.kraken.com/hc/en-us/articles/360001185506-How-to-interpret-asset-codes
+        """
+        if ticker_pair:
+            params = {
+                "pair": ticker_pair
+            }
+        else:
+            params = None
+
+        return await self._get_public("Ticker", params=params)
+
+    async def get_ohlc_data(self, ticker_pair: str, since: int, minute_interval: int = 1):
+        """Ticker pair is a valid combination of asset codes as outlined in kraken support
+
+        https://support.kraken.com/hc/en-us/articles/360001185506-How-to-interpret-asset-codes
+        """
+        params = {
+            "pair": ticker_pair,
+            "interval": minute_interval,
+            "since": since
+        }
+
+        return await self._get_public("OHLC", params=params)
+
+    async def get_order_book(self, ticker_pair: str, count: int = 100):
+        params = {
+            "pair": ticker_pair,
+            "count": count
+        }
+
+        return await self._get_public("Depth", params=params)
+
+    async def get_recent_trades(self, ticker_pair: str, since: int, count: int = 100):
+        params = {
+            "pair": ticker_pair,
+            "since": since,
+            "count": count
+        }
+
+        return await self._get_public("Trades", params=params)
+
+    async def get_recent_spreads(self, ticker_pair: str, since: int):
+        params = {
+            "pair": ticker_pair,
+            "since": since
+        }
+
+        return await self._get_public("Spread", params=params)
--- kraken/test.py
+++ kraken/test.py
@@ -1,13 +1,49 @@
 import asyncio
+from time import time
 
 from kraken.client import KrakenClient
 
 kraken_client = KrakenClient()
 
+to_test = [
+    kraken_client.get_server_time,
+    kraken_client.get_assets_info,
+    kraken_client.get_system_status
+]
 
-async def get_time_test():
-    print(await kraken_client.get_server_time())
+
+async def test_assets_info():
+    print(await kraken_client.get_assets_info(assets=["eth", "sol"]))
+
+
+async def test_tradable_pairs():
+    print(await kraken_client.get_tradable_asset_pairs(["BTC/USD"]))
+
+
+async def test_get_ticker_info():
+    print(await kraken_client.get_ticker_information("ETHUSD"))
+
+
+async def test_get_ohlc():
+    print(await kraken_client.get_ohlc_data("ETHUSD", int(time() - 540)))
+
+
+async def test_get_order_book():
+    print(await kraken_client.get_order_book("ETHUSD", count=20))
+
+
+async def test_get_recent_trades():
+    print(await kraken_client.get_recent_trades("ETHUSD", since=int(time() - 540), count=50))
+
+
+async def test_get_recent_spread():
+    print(await kraken_client.get_recent_spreads("ETHUSD", since=int(time() - 540)))
+
+
+async def test_all():
+    for func in to_test:
+        print(await func())
 
 
 if __name__ == "__main__":
-    asyncio.run(get_time_test())
+    asyncio.run(test_get_recent_spread())
